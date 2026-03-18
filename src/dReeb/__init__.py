d3b40c03c0b45dd2c705f3375e613d251ab7b408
Zack Warren <zackmanalowarren@gmail.com>
2026-03-18 01:15:58 -0700

    fix package folder case

--- src/dReeb/__init__.py
+++
@@ -1,5 +0,0 @@
-from .dreeb import dreeb
-from .graph import build_affinity_matrix, build_diffusion_operator
-from .filter import find_endpoints, compute_filter
-from .reeb import prepare_reeb, build_reeb_graph
-from .simplify import simplify_reeb_graph
